480c6d4c5938ebc452fa0e4360a116968adc71aa
Remy Sharp <remy@remysharp.com>
2019-09-11 16:33:34 +0100

    wider

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const FRAME_RATE = 100;
 export const WIDTH = 50;
 export const HEIGHT = 50;
 
-const height = typeof window !== 'undefined' ? window.innerHeight : 100;
+const width = typeof window !== 'undefined' ? window.innerWidth : 100;
 
-export const PIXEL = ((height / HEIGHT) * 0.6) | 0;
+export const PIXEL = ((width / WIDTH) * 0.9) | 0;
