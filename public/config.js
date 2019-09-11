eeb6e9ecb76e17b00f37aaef6a92a874a51d3cc6
Remy Sharp <remy@remysharp.com>
2019-09-11 16:32:43 +0100

    remove build dir

--- public/config.js
+++
@@ -1,7 +0,0 @@
-export const FRAME_RATE = 100;
-export const WIDTH = 50;
-export const HEIGHT = 50;
-
-const height = typeof window !== 'undefined' ? window.innerHeight : 100;
-
-export const PIXEL = ((height / HEIGHT) * 0.6) | 0;
